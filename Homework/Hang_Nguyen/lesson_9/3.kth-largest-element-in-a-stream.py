189f0489274ee30453e525d6cb36b8576d5c6ef2
codeeMadness <codee.madness@gmail.com>
2023-04-10 23:25:16 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_9/3.kth-largest-element-in-a-stream.py
+++ Homework/Hang_Nguyen/lesson_9/3.kth-largest-element-in-a-stream.py
@@ -1,1 +1,26 @@
 # https://leetcode.com/problems/kth-largest-element-in-a-stream/
+import heapq
+
+class KthLargest(object):
+    def __init__(self, k, nums):
+        self.pool = nums
+        self.k = k
+        heapq.heapify(self.pool)
+        while len(self.pool) > k:
+            heapq.heappop(self.pool)
+
+            
+    def add(self, val):
+        heapq.heappush(self.pool, val)
+        if len(self.pool) > self.k:
+            heapq.heappop(self.pool)
+        return self.pool[0]
+
+# TEST CASE
+kthLargest = KthLargest(3, [4, 5, 8, 2])
+
+print(kthLargest.add(3))
+print(kthLargest.add(5))
+print(kthLargest.add(10))
+print(kthLargest.add(9))
+print(kthLargest.add(4))
