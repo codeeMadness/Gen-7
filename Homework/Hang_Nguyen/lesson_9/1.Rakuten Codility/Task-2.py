da6f0b3e72651de18e1728972cdf8517fea7fb12
codeeMadness <codee.madness@gmail.com>
2023-04-12 21:20:25 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_9/1.Rakuten Codility/Task-2.py
+++ Homework/Hang_Nguyen/lesson_9/1.Rakuten Codility/Task-2.py
@@ -1,1 +1,17 @@
 # https://drive.google.com/file/d/1FbGlgYd0cqLzcd-HI0ecKIPceGlULwDb/view?fbclid=IwAR2DUw7unGezSWCW4G8B5MJNWiGvY8j5u_adecnrxr_m0dqp_HexVcxdhi4
+
+class Solution:
+    def findWinter(self, T):
+        res = 0
+        l, r = 0, len(T) - 1
+        return res
+
+# TEST CASE
+solution = Solution()
+
+T = [5, -2, 3, 8, 6]
+print(solution.findWinter(T))
+
+T = [-5, -5, -5, -42, 6, 12]
+print(solution.findWinter(T))
+
