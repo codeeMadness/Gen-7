59bdd47e626631161275a831be991a942c1c0295
codeeMadness <codee.madness@gmail.com>
2023-04-13 10:38:49 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_9/1.Rakuten Codility/Task-2.py
+++ Homework/Hang_Nguyen/lesson_9/1.Rakuten Codility/Task-2.py
@@ -1,17 +1,31 @@
 # https://drive.google.com/file/d/1FbGlgYd0cqLzcd-HI0ecKIPceGlULwDb/view?fbclid=IwAR2DUw7unGezSWCW4G8B5MJNWiGvY8j5u_adecnrxr_m0dqp_HexVcxdhi4
 
 class Solution:
-    def findWinter(self, T):
-        res = 0
+    def minimumLengthOfWinter(self, T):
         l, r = 0, len(T) - 1
-        return res
+        peakInWinter = -999999
+
+        while l <= r:
+            if T[l] < T[r]:
+                length = r
+                r -= 1
+            else:
+                peakInWinter = max(peakInWinter, T[l])
+                l += 1
+
+        return length
 
 # TEST CASE
 solution = Solution()
 
 T = [5, -2, 3, 8, 6]
-print(solution.findWinter(T))
+print(solution.minimumLengthOfWinter(T))
 
 T = [-5, -5, -5, -42, 6, 12]
-print(solution.findWinter(T))
+print(solution.minimumLengthOfWinter(T))
 
+T = [0, 1, 2, 3, 4]
+print(solution.minimumLengthOfWinter(T))
+
+T = [3, 9, 0, 4, 8, 10]
+print(solution.minimumLengthOfWinter(T))
