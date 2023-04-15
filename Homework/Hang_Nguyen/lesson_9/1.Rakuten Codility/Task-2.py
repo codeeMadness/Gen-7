0cc9087b4e788634849bdcf7807a73f6e6a1c830
codeeMadness <codee.madness@gmail.com>
2023-04-15 19:19:58 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_9/1.Rakuten Codility/Task-2.py
+++ Homework/Hang_Nguyen/lesson_9/1.Rakuten Codility/Task-2.py
@@ -2,8 +2,25 @@
 
 class Solution:
     def findWinter(self, T):
-        res = 0
+        res = len(T)
         l, r = 0, len(T) - 1
+        highestWinter = T[l]
+        while  l <= r :
+            if T[l] < T[r]:
+                r -= 1
+                res -= 1
+            else:
+                highestWinter = max(highestWinter, T[l])
+                l += 1
+            
+        l, r = r + 1, len(T) - 1
+        while l <= r:
+            if highestWinter > T[l]:
+                res += 1
+                l += 1
+            else:
+                r = -1
+
         return res
 
 # TEST CASE
@@ -13,5 +30,11 @@ T = [5, -2, 3, 8, 6]
 print(solution.findWinter(T))
 
 T = [-5, -5, -5, -42, 6, 12]
+print(solution.findWinter(T))
+
+T = [0, 1, 2, 3, 4]
+print(solution.findWinter(T))
+
+T = [4, 9, 3, 8, 7, 6, 10]
 print(solution.findWinter(T))
 
