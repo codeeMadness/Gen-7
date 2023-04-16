ece5e557da01c2a257a2f672301dd0cb909d006b
codeeMadness <codee.madness@gmail.com>
2023-04-16 21:12:17 +0700

    submit hw

--- Homework/Hang_Nguyen/lesson_9/2.largest-component-size-by-common-factor.py
+++ Homework/Hang_Nguyen/lesson_9/2.largest-component-size-by-common-factor.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/largest-component-size-by-common-factor/
+class Solution():
+    def largestComponentSize(self, nums):
+        return len(nums)
+
+# TEST CASE
+solution = Solution()
+
+nums = [4,6,15,35]
+print(solution.largestComponentSize(nums))
+nums = [20,50,9,63]
+print(solution.largestComponentSize(nums))
+nums = [2,3,6,7,4,12,21,39]
+print(solution.largestComponentSize(nums))
